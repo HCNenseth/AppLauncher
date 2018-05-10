b68596a15d6dffabacd1e2dc4c6c231211567c8c
Hans Christian Nenseth <nenseth@gmail.com>
2018-05-10 14:43:23 +0200

    First commit

---
+++ app/src/main/java/tech/darksun/nerdlauncheer/NerdLauncherActivity.java
@@ -0,0 +1,11 @@
+package tech.darksun.nerdlauncheer;
+
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
